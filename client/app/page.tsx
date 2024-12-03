9145f40c89449de1bff3bfdfb123572519b90a25
德华 刘 <gavinprior5@gmail.com>
2024-12-03 19:07:10 +0000

    import Navigation Bar

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,8 @@
+import NavigationBar from "@/components/nav-menu/NavigationBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <><NavigationBar/></>
   );
 }
