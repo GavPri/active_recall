1fa56d6526d679f519d61c7d1c288b30fb2b8bb4
德华 刘 <gavinprior5@gmail.com>
2024-12-03 19:12:35 +0000

    import and add necessary shadcn comps.

--- client/components/nav-menu/NavigationBar.tsx
+++ client/components/nav-menu/NavigationBar.tsx
@@ -1,10 +1,30 @@
-"use client"
-import React from 'react'
+"use client";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import Link from "next/link";
+import React from "react";
 
 const NavigationBar = () => {
   return (
-    <div className='w-full h-24 shadow-sm'>NavigationBar</div>
-  )
-}
+    <div className="w-full h-24 shadow-sm">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="#">
+              <NavigationMenuLink>
+                Sign Up
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
